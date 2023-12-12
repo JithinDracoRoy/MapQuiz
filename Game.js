8dd768f54f20d1f4f15392198c95db1d813f66a3
Pavithra820 <56164471+Pavithra820@users.noreply.github.com>
2023-12-12 15:30:34 +0530

    Add files via upload
    Adding my question Function

---
+++ Game.js
@@ -0,0 +1,27 @@
+// let questions = ["Which continent is India part of?",
+//                 "Which is the largest continent in The World?",
+//                 "Which is the smallest continent in area?",
+//                 "Which continent is also called as an 'Island-Continent'?",
+//                 "The largest mountain in the world, Mount Everest is situated in which continent?",
+//                 "On Which Continent Is Egypt?",
+//                 "On which continent can you find France?",
+//                 "Which is the Second biggest continent of the world?",
+//                 "Columbus found which continent?",
+//                 "Where is Amazon River?"];
+var x=0;
+function questions(){
+    document.getElementById('a').innerHTML("");
+    document.getElementById('b').innerHTML("");
+    document.getElementById('m').style.pointerEvents("auto")
+    document.getElementById('n').style.pointerEvents("none")
+    var randomNumber= Math.floor((Math.random() * 10) + 1); 
+
+ for(i=0;x<i;i++){
+   if(used[i]==randomNumber){
+   i=0;
+randomNumber=Math.floor((Math.random()*10)+1);
+ }
+}
+used.push(randomNumber)
+document.getElementById("q1").innerHTML=questions[randomnumber];
+}

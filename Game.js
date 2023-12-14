62914cedd2a41b449ff8e660dbf5385b3d54ffb7
jinturenji <152945418+jinturenji@users.noreply.github.com>
2023-12-14 11:53:19 +0530

    Update Game.js

--- Game.js
+++ Game.js
@@ -52,3 +52,21 @@ function check(a){
         fin();
     }
 }
+    let finalOutput= () => {
+
+    document.getElementById("myBtn").disabled = true; 
+    let name = localStorage.getItem(key);
+    let noOfQuestions = localStorage.getItem(key);
+    let score = (mark/noOfQuestions)*100;
+    if( mark > 50 ){
+        document.getElementById("id").innerHTML = "Congratulations" + name + ", You have scored " + score + "%.";
+        document.getElementById("id").style.color = "green";
+    }
+    else{
+
+        document.getElementById("id").innerHTML = "Opps" + name + ", You have scored below 50%.";
+        document.getElementById("id").style.color = "red";
+
+    }
+
+  } 
